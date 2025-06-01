f6d1ab5f8bae8be936903c3c16532bcb07fa1332
CuiLiang <cuiliang.bj@gmail.com>
2025-06-01 14:34:32 +0800

    修复，无法输出nuget包的问题。

--- publish-local-package.bat
+++ publish-local-package.bat
@@ -1,4 +1,8 @@
+cd /d "%~dp0"
+
 dotnet pack .\src\ManagedShell\ManagedShell.csproj -c Release ^
+ -p:AssemblyOriginatorKeyFile=d:\cert\cert2022\quicker.snk ^
+ -p:SignAssembly=true ^
  -o ..\_nupkgs
 
  pause
